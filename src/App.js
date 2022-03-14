9e6ed0df97e9a5ebdd3d02147e1cc5c6d344148a
Sudhan Maharjan <mrjnsudhan@gmail.com>
2022-03-14 22:37:23 +1100

    added text in feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yes did it!!! Edit <code>src/App.js</code> and save to reload.
+          Changed in feature branch Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
